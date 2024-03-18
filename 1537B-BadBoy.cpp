818aae6afe1036ede8ebad815a2cb53f4d2ca6c5
vivekreddy1601 <130446079+vivekreddy1601@users.noreply.github.com>
2024-03-18 14:41:11 +0530

    1537B-BadBoy.cpp

---
+++ 1537B-BadBoy.cpp
@@ -0,0 +1,16 @@
+//code by Vivek Reddy
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ll vt;
+    cin >> vt;
+    while(vt--){
+        ll n,m,i,j;
+        cin >> n>> m >> i >> j;
+        cout << 1 << ' ' << 1 << ' ' << n << ' ' << m << '\n';
+    }
+    return 0;
+}
