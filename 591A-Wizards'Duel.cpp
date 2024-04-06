ef1ecaa6c034ddccd980567032cea709f97e4e2b
vivekreddy1601 <130446079+vivekreddy1601@users.noreply.github.com>
2024-04-06 15:24:09 +0530

    591A-Wizards'Duel.cpp

---
+++ 591A-Wizards'Duel.cpp
@@ -0,0 +1,12 @@
+//code by Vivek Reddy
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double l,p,q,ans=0;
+    cin >> l >> p >> q;
+    ans = (l/(p+q))*p;
+    cout << ans << '\n';
+    return 0;
+}
