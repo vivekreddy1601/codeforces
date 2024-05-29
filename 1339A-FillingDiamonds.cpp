f88e51ebe0981c5e75d2c31bc0fb9bcca9053233
vivekreddy1601 <130446079+vivekreddy1601@users.noreply.github.com>
2024-05-29 23:34:21 +0530

    1339A-FillingDiamonds.cpp

---
+++ 1339A-FillingDiamonds.cpp
@@ -0,0 +1,14 @@
+//code by Vivek Reddy
+#include<iostream>
+using namespace std;
+int main()
+{
+    int vt;
+    cin >> vt;
+    while(vt--){
+        int n;
+        cin >> n;
+        cout << n << endl;
+    }
+    return 0;
+}
