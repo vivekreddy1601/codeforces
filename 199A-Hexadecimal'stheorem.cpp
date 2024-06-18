9a27bb75361a0d54c6e4b989f597533b6ae09215
vivekreddy1601 <130446079+vivekreddy1601@users.noreply.github.com>
2024-06-18 09:04:13 +0530

    199A-Hexadecimal'stheorem.cpp

---
+++ 199A-Hexadecimal'stheorem.cpp
@@ -0,0 +1,12 @@
+//code by Vivek Reddy
+#include <iostream>
+#define ll long long 
+using namespace std;
+
+int main()
+{
+    ll n;
+    cin >> n;
+    cout << 0 << ' ' << 0 << ' ' << n << '\n';
+    return 0;
+}
