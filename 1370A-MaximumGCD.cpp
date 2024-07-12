fdfbf137addf254bc8362c014990513dcc3a100f
vivekreddy1601 <130446079+vivekreddy1601@users.noreply.github.com>
2024-07-12 10:17:58 +0530

    1370A-MaximumGCD.cpp

---
+++ 1370A-MaximumGCD.cpp
@@ -0,0 +1,15 @@
+//code by Vivek Reddy
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int vt;
+    cin >> vt;
+    while(vt--){
+        int n;
+        cin >> n;
+        cout << n/2 << '\n';
+    }
+    return 0;
+}
