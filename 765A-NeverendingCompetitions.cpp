e6ab847ecd22ce0eed0ffb6a92c2741bfa30c4e7
vivekreddy1601 <130446079+vivekreddy1601@users.noreply.github.com>
2024-09-30 23:20:58 +0530

    765A-NeverendingCompetitions.cpp

---
+++ 765A-NeverendingCompetitions.cpp
@@ -0,0 +1,19 @@
+//code bt Vivek Reddy
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    string s;
+    cin >> n >> s;
+    for(int i = 0;i < n;i++){
+        string t;
+        cin >> t;
+    }
+    if(n%2==0)
+        cout << "home\n";
+    else
+        cout << "contest\n";
+    return 0;
+}
