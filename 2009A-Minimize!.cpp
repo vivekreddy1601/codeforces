8aa41c8c47f2a620bd2cacd9f1af2aa1d6129fcd
vivekreddy1601 <130446079+vivekreddy1601@users.noreply.github.com>
2024-10-27 19:33:46 +0530

    2009A-Minimize!.cpp

---
+++ 2009A-Minimize!.cpp
@@ -0,0 +1,15 @@
+//code by Vivek Reddy
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int vt;
+    cin >> vt;
+    while(vt--){
+        int a,b;
+        cin >> a >> b;
+        cout << b-a << '\n';
+    }
+    return 0;
+}
